a83edfccd83d2bf491dc28bf42f0e0e3f2292d73
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-04-13 00:58:19 +0530

    added shell script

--- point.h
+++ point.h
@@ -16,6 +16,6 @@ struct Point
 {
   int x;
   int y;
-} ;
+};
 
 void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location);
---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rm -rf *.o
+echo Compiling...
+gcc -c *.c
+if [ $? != 0 ]
+then 
+echo ----failed----
+exit
+fi
+echo Linking...
+gcc -o a.out *.o && ./a.out
+echo ...done
+rm -rf *.o

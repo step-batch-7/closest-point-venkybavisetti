afd5e5ec89809116a99c02022517d832c613a7b1
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-04-13 01:24:43 +0530

    added connections b/w manin.c,point.h and point.c

---
+++ point.c
@@ -0,0 +1,5 @@
+#include "point.h"
+
+void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location)
+{
+}
--- point.h
+++ point.h
@@ -12,10 +12,10 @@ Location of organism: [69 7],   Closest food target : [55 38]
 Location of organism: [10 94],  Closest food target : [18 76]
 Location of organism: [36 27],  Closest food target : [55 38]
 */
-struct Point
+typedef struct
 {
   int x;
   int y;
-};
+} POINT;
 
-void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location);
+void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location);
